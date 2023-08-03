029fedd8a172d292feedd8c6badbc58138405263
ChaseRensberger <chasehrensberger@gmail.com>
2023-08-02 21:12:50 -0400

    remake portfolio

---
+++ src/components/Contact.tsx
@@ -0,0 +1,110 @@
+"use client";
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
+import Link from "next/link";
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Contact() {
+  const [agreed, setAgreed] = useState(false);
+  return (
+    <section className="w-screen h-screen flex items-center justify-center snap-center">
+      <div className="bg-black px-6 py-24 sm:py-32 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="font-bold tracking-tight text-white text-5xl">
+            Contact.
+          </h2>
+          <p className="mt-5 text-2xl leading-8 text-white">
+            For portfolio questions, freelance projects, and more.
+          </p>
+        </div>
+        <form
+          action="#"
+          method="POST"
+          className="mx-auto mt-4 max-w-2xl sm:mt-12"
+        >
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div>
+              <label
+                htmlFor="first-name"
+                className="block text-lg font-semibold leading-6 text-white"
+              >
+                First name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="last-name"
+                className="block text-lg font-semibold leading-6 text-white"
+              >
+                Last name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="email"
+                className="block text-lg font-semibold leading-6 text-white"
+              >
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block text-lg font-semibold leading-6 text-white"
+              >
+                Message
+              </label>
+              <div className="mt-2.5">
+                <textarea
+                  name="message"
+                  id="message"
+                  rows={4}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                  defaultValue={""}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mt-10">
+            <button
+              type="submit"
+              className="block w-full rounded-md bg-blue-500 px-3.5 py-2.5 text-center text-lg font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+            >
+              Let's talk
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/ContentSection.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { useState, useEffect, useRef } from "react";
+import { useInView } from "framer-motion";
+
+interface Props {
+  videoUrlHorizontal: string;
+  videoUrlVertical: string;
+}
+
+export default function ContentSection({
+  videoUrlHorizontal,
+  videoUrlVertical,
+}: Props) {
+  const videoRef = useRef<any>(null);
+  const isInView = useInView(videoRef);
+
+  const [windowDimension, setWindowDimension] = useState({
+    winWidth: window.innerWidth,
+    winHeight: window.innerHeight,
+  });
+
+  const detectSize = () => {
+    setWindowDimension({
+      winWidth: window.innerWidth,
+      winHeight: window.innerHeight,
+    });
+  };
+
+  const getCorrectPath = () => {
+    return windowDimension.winWidth >= windowDimension.winHeight
+      ? videoUrlHorizontal
+      : videoUrlVertical;
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", detectSize);
+    if (videoRef.current) {
+      videoRef.current.load();
+    }
+
+    return () => {
+      window.removeEventListener("resize", detectSize);
+    };
+  }, [windowDimension]);
+
+  useEffect(() => {
+    if (isInView && videoRef.current) {
+      videoRef.current.play();
+    } else if (!isInView && videoRef.current) {
+      videoRef.current.pause();
+    }
+  }, [isInView]);
+
+  return (
+    <section className="w-screen h-screen flex items-center justify-center snap-center">
+      <video
+        autoPlay
+        muted
+        playsInline
+        width={1000}
+        ref={videoRef}
+        className="w-full h-full"
+      >
+        <source src={getCorrectPath()} type="video/mp4" />
+      </video>
+    </section>
+  );
+}
